6f26e9baacbab510bdc348d22e7af6b24224cb49
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-10-23 11:56:11 -0500

    Tercer commit3 - tests corregidos exitosamente

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import unittest
 
 class TestSuma(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(2 + 2, 5)  # ❌ Error intencional
+        self.assertEqual(2 + 2, 4)  #Corregido
 
 class TestTexto(unittest.TestCase):
     def test_texto(self):
-        self.assertEqual("Hola", "Chao")  # ❌ Error intencional
+        self.assertEqual("Hola", "Hola")  #Corregido
 
 if __name__ == '__main__':
     unittest.main()
